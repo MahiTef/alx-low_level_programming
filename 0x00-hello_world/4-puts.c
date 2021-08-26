49513f68ba21286223b0558c38b66e6d205d0729
MahiTef <mahletteferi9@gmail.com>
2021-08-26 05:58:53 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+/* This is a program that prints Progamming is like building a multilingual puzzle */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
