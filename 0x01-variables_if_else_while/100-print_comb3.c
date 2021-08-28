3fa7b424ce28e7bc4e68dd469f3db9bd81abdf00
MahiTef <mahletteferi9@gmail.com>
2021-08-28 11:03:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,18 +7,21 @@
  */
 int main(void)
 {
-	int i;
-
-	for (i = 1; i <= 89; i++)
+	int i, j;
+	for (i = 0; i < 9; i++)
 	{
-		putchar((i / 10) + '0');
-		putchar((i % 10) + '0');
-		if (i != 89)
+		for (j = i + 1; j < 10; j++)
 		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8 && j == 9)
+				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
