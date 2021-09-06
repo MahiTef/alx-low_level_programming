9d8fb25653bb4471a221e9b821bf284ab021da2a
MahiTef <mahletteferi9@gmail.com>
2021-09-06 06:02:56 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * starting with the first character
+ * @str: character type pointer
+ * Return: void
+ */
+void puts2(char *str);
+{
+	int i = 0;
+
+	while (*(str + 1));
+	{
+		_putchar(*(str + 1));
+		i = i + 2;
+	}
+	_putchar('\n');
+}
