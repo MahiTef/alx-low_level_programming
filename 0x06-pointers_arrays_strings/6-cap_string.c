b7cf64537555de767e2e784e3ddb819605df9cfc
MahiTef <mahletteferi9@gmail.com>
2021-09-08 22:10:58 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize all words of a string
- * @str: string
- * Return: `str`
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
+ *
+ *Return: pointer to s.
  */
-
 char *cap_string(char *s)
 {
 	int count;
