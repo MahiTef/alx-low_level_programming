b4559481758e4fc05c93c6cd39ee9c7bc4c370a3
MahiTef <mahletteferi9@gmail.com>
2021-09-29 10:56:51 -0700

    3-calc.h

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,73 +1,70 @@
 #include "3-calc.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * op_add - adds a and b
- * @a: first number
- * @b: second number
- *
- * Return: sum of a and b
- */
+  *op_add - return the sum of 2 numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: sum.
+  */
 int op_add(int a, int b)
 {
-return (a + b);
+	return (a + b);
 }
 
 /**
- * op_sub - subtracts b from a
- * @a: first number
- * @b: second number
- *
- * Return: difference of a and b
- */
+  *op_sub - returns difference of 2 numbers
+  *@a:  integer.
+  *@b: integer.
+  *
+  *Return: difference.
+  */
 int op_sub(int a, int b)
 {
-return (a - b);
+	return (a - b);
 }
 
 /**
- * op_mul - multiplies a and b
- * @a: first number
- * @b: second number
- *
- * Return: multiplication of a and b
- */
+  *op_mul - returns product of two numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: product.
+  */
 int op_mul(int a, int b)
 {
-return (a * b);
+	return (a * b);
 }
 
 /**
- * op_div - divides a by b
- * @a: first number
- * @b: second number
- *
- * Return: integer division of a by b
- */
+  *op_div - returns the quotient of 2 numbers.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: quotient.
+  */
 int op_div(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-return (a / b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
 }
 
 /**
- * op_mod - find the remainder of the division of a by b
- * @a: first number
- * @b: second number
- *
- * Return: remainder of the division of a by b
- */
+  *op_mod - returns the remainder of division.
+  *@a: integer.
+  *@b: integer.
+  *
+  *Return: remainder.
+  */
 int op_mod(int a, int b)
 {
-if (b == 0)
-{
-printf("Error\n");
-exit(100);
-}
-return (a % b);
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
