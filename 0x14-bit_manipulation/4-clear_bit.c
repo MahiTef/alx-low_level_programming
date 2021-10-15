3cab76eb94b0a8e217a8d5e16240aae8dab89829
MahiTef <mahletteferi9@gmail.com>
2021-10-14 19:32:47 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,40 +1,21 @@
-#include  "main.h"
-/**
-  *getp - returns the value of x to the power of y.
-  *@x: number.
-  *@y: power.
-  *
-  *Return: x to the power of y.
-  */
-int getp(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	if (y == 1)
-		return (x);
-	return (x * getp(x, y - 1));
-}
+#include <stdio.h>
+#include "main.h"
 
 /**
-  *clear_bit - stes the value of a bit to one at a given index.
-  *@n: pointer to number.
-  *@index: index of bit.
-  *
-  *Return: 1 if it worked, -1 if it did not.
-  */
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: decimal number
+ * @index: index
+ * Return: 1 if it worked or -1 if an error occurred
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int mask, var;
+
+	var = 0;
+	mask = 1;
 	if (index > 63)
 		return (-1);
-	if (n)
-	{
-		*n -= getp(2, index);
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+	var = *n & ~(mask << index);
+	*n = var;
+	return (1);
 }
