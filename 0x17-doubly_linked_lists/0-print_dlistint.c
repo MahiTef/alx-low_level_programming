f68e4d30bcaace934a6f13d7e6323017cc149cb8
MahiTef <mahletteferi9@gmail.com>
2021-11-25 10:23:08 -0800

    README.md

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,18 +7,18 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  int nodes = 0;
-  dlistint_t *tmp = (dlistint_t *)h;
+int nodes = 0;
+dlistint_t *tmp = (dlistint_t *)h;
 
-  if (h)
-    {
-      while (tmp)
-	{
-	  printf("%d\n", tmp->n);
-	  nodes++;
-	  tmp = tmp->next;
-	}
-    }
+if (h)
+{
+while (tmp)
+{
+printf("%d\n", tmp->n);
+nodes++;
+tmp = tmp->next;
+}
+}
 
-  return (nodes);
+return (nodes);
 }
